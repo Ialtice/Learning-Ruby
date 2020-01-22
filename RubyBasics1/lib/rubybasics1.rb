49d17e31efd81ab7d3ae456996fa7e1b3b52ed12
Ialtice <ialtice@uncc.edu>
2020-01-22 18:11:54 +0000

    RubyBasics1 - Part2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -12,7 +12,30 @@ end
 
 # Part II
 def max_2_sum arr
- 
+  index = 2
+  total = 0
+  max1 = arr[0]
+  max2 = arr[1]
+  if arr.length == 0
+    
+  elsif arr.length == 1
+    total = max1
+    
+  else
+    while index < arr.length
+    if max1 < arr[index]
+      max1 = arr[index]
+    end
+    if max2 < arr[index]
+      max2 = arr [index]
+    end
+    index += 1
+    end
+    total += max1
+    total += max2
+    
+  end
+  return total
 end
 
 # Part III
