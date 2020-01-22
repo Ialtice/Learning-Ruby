2a0ef3655beb900bdc61c02725e8a69be14e9d71
Ialtice <ialtice@uncc.edu>
2020-01-22 17:54:39 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,21 @@
+# Lab 1
+# Part I
+def sum arr
+  index = 0
+  total = 0
+  while index < arr.length
+    total += arr[index]
+    index += 1
+  end
+  return total
+end
+
+# Part II
+def max_2_sum arr
+ 
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
