50b133591de263e5e365504134e4bbedf2b5d4b0
Ialtice <ialtice@uncc.edu>
2020-01-26 16:18:48 +0000

    RubyHashes - Part1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,12 +1,20 @@
 # RubyHashes
 # Part I
 def array_2_hash emails, contacts
-    # YOUR CODE HERE
+    if emails.empty?
+        
+    else
+        contacts['Bob Smith'] = emails[0]
+        contacts['Sally Field'] =  emails[1]
+        contacts['Mark Dole'] =  emails[2]
+    end 
+    return contacts
+    
 end
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+    
 end
 
 # Part III
