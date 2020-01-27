7d69215ae4501821c75451ba533e4c503ddf4c86
Ialtice <ialtice@uncc.edu>
2020-01-27 03:14:14 +0000

    RubyHashes - Part 3

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -33,6 +33,18 @@ def array2d_2_hash contact_info, contacts
 end
 
 # Part III
-def hash_2_array(contacts)
+def hash_2_array contacts
+    array = Array.new([[],[],[]])
     
+    if contacts == {}
+        
+    else
+        array[0][0] = contacts[contacts.keys[0].to_sym][contacts.values[0].keys[0]]
+        array[0][1] = contacts[contacts.keys[1].to_sym][contacts.values[1].keys[0]]
+        array[1][0] = contacts[contacts.keys[0].to_sym][contacts.values[1].keys[1]]
+        array[1][1] = contacts[contacts.keys[1].to_sym][contacts.values[1].keys[1]]
+        array[2][0] = contacts.keys[0].to_s
+        array[2][1] = contacts.keys[1].to_s
+    end
+    return array
 end
