f97a0bc801fdfb97be6aa9077577ea8346cecc81
Ialtice <ialtice@uncc.edu>
2020-01-27 02:35:03 +0000

    RubyHashes - Part 2

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -16,30 +16,23 @@ end
 
 # Part II
 def array2d_2_hash contact_info, contacts
+    array = contacts.keys
     if contact_info == [[]]
-       
-    
+        contacts = {
+                array[0].to_sym =>  {},
+                array[1].to_sym =>  {}
+            }
     else
         contacts = {
-                :'Bob Smith' =>  {:email => contact_info[0][0] , :phone => contact_info[0][1]},
-                :'Sally Field' =>  {:email => contact_info[1][0] , :phone => contact_info[1][1]}
+                array[0].to_sym =>  {:email => contact_info[0][0] , :phone => contact_info[0][1]},
+                array[1].to_sym =>  {:email => contact_info[1][0] , :phone => contact_info[1][1]}
             }
     end
     return contacts
+
 end
 
 # Part III
 def hash_2_array(contacts)
-    array = Array.new([[],[],[]])
-    if contacts == {}
-        
-    else
-        array[0][0] = contacts[:'Bob Smith'][:email]
-        array[0][1] = contacts[:'Sally Field'][:email]
-        array[1][0] = contacts[:'Bob Smith'][:phone]
-        array[1][1] = contacts[:'Sally Field'][:phone]
-        array[2][0] = contacts.keys[0].to_s
-        array[2][1] = contacts.keys[1].to_s
-    end
-    return array
+    
 end
