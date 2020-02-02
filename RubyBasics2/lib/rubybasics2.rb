ee818029d99c15ada417d00a805213cd44e939c5
Ialtice <ialtice@uncc.edu>
2020-02-02 18:34:15 +0000

    Ruby Basics 2 – Part 3

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -24,5 +24,26 @@ end
 
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  i = 0
+  isBinary = true
+  isDivisBy4 = false
+  if(s.empty?)
+    puts "The empty string is not a valid binary number!"
+  else
+    while(i < s.length)
+      if ! (s[i].eql?("0") || s[i].eql?("1"))
+        isBinary = false
+      end  
+      i+=1
+    end
+    if(isBinary == false)
+      puts "\"" + s  + "\"" + " is not a valid binary number!"
+    else
+      if (s.eql?("0") || s.end_with?("00"))
+        isDivisBy4 = true
+      end
+      
+  end
+    
+  end
 end
