6cc087501f6d9dc2e7eeaad59be0246085d4e783
Ialtice <ialtice@uncc.edu>
2020-02-11 16:35:54 +0000

    Lab 9: Fun With Words – Palindrome

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -6,13 +6,12 @@ module FunWithStrings
   def count_words
 
   end
-  
   def anagram_groups
-  
+   
   end
+end
 # make all the above functions available as instance methods on Strings:
 
 class String
   include FunWithStrings
-end
 end
