66b00bfcb4f0fb546b5d3d11338658b987531340
Ialtice <ialtice@uncc.edu>
2020-02-11 16:42:22 +0000

    Lab 9: Fun With Words – count words

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -4,7 +4,15 @@ module FunWithStrings
     
   end
   def count_words
-
+    countWordsHash = Hash.new
+        self.downcase.gsub(/[^a-z\s]/,'').split.each do |word|
+          if countWordsHash[word] == nil
+            countWordsHash[word] = 1
+          else 
+            countWordsHash[word] += 1
+          end
+        end
+    countWordsHash
   end
   def anagram_groups
    
