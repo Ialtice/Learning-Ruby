268cbd8776502a01a4f9cbc81054eafe7cfd0a01
Ialtice <ialtice@uncc.edu>
2020-02-17 00:47:25 +0000

    Ruby OO Basics – Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,12 @@
 # Ruby Basics Part 3
 
-class BookInStock
+class BookInStock 
+    attr_accessor :isbn, :price
+    attr_reader :isbn,:price
+    attr_writer :isbn, :price
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = price
+    end
 # YOUR CODE HERE
 end
